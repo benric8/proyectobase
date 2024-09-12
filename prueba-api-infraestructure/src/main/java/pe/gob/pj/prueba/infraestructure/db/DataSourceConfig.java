db050e8306cc88edcb353b5aa5c9d5a946dcec59
Ricardo Ruiz Benites <oricardorb@gmail.com>
2024-09-12 08:32:42 -0500

    feature: cambiado a java 21 con SessionFactory

--- prueba-api-infraestructure/src/main/java/pe/gob/pj/prueba/infraestructure/db/DataSourceConfig.java
+++ prueba-api-infraestructure/src/main/java/pe/gob/pj/prueba/infraestructure/db/DataSourceConfig.java
@@ -7,8 +7,6 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
